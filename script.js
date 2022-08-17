a9e2301b5261cf0fd5109f36239199c6e64e970a
Denis Homoki <homoki.denisth@gmail.com>
2022-08-17 14:22:44 +0300

    finish app

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-$(function () {});
+$(function () {
+  let count = 0;
+
+  $("#btn-increment").click(function () {
+    count += 1;
+    $("#people").text(count);
+  });
+
+  $("#btn-save").click(function () {
+    $("#previous-people").append(count + " - ");
+    $("#people").text(0);
+    count = 0;
+  });
+});
